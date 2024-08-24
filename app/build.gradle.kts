0948f119188b0e12bc3272bd945f03149c38c9e1
jubo445 <juboraj335.ja@gmail.com>
2024-08-24 17:35:08 +0600

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
+    alias(libs.plugins.hiltAndroid)
+}
+
+android {
+    namespace = "com.creativeitinstitute.mvvmcleanecomrestapi"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.creativeitinstitute.mvvmcleanecomrestapi"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //   retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.retrofit.okhttp)
+
+    // dagger hilt
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    //coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // lifecycle ViewModel
+    implementation(libs.android.viewmodel)
+    implementation(libs.android.livedata)
+    implementation(libs.fragment.ktx)
+
+    //navigation kotlin
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    //coil
+    implementation(libs.coil)
+
+//    image picker
+//    implementation(libs.imagepicker)
+
+    //
+    implementation(libs.paging)
+
+
+
+}
---
+++ app/src/main/java/com/creativeitinstitute/mvvmcleanecomrestapi/MyApp.kt
@@ -0,0 +1,8 @@
+package com.creativeitinstitute.mvvmcleanecomrestapi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp:Application() {
+}
